5e7b5542b69be5fd1122b8492912cb5c8322a515
chuqian <chuqian@idengyun.com>
2020-05-23 14:33:03 +0800

    修改启动脚本

--- run/start.sh
+++ run/start.sh
@@ -4,5 +4,5 @@ cd '你的项目地址'
 mvn clean package -Dmaven.test.skip=true
 cd run
 rm -rf pan.log pad.txt
-nohup ../target/r-pan-1.0.jar > pan.log 2>&1&
+nohup java -jar ../target/r-pan-1.0.jar > pan.log 2>&1&
 echo $! > pid.txt
